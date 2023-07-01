905db9302cb72c3cbe0b3b0161eb24772a718b43
moctran <87555673+moctran@users.noreply.github.com>
2023-07-01 15:40:07 +0700

    Add files via upload

--- FinalAsm/Dice.java
+++ FinalAsm/Dice.java
@@ -6,13 +6,57 @@ public class Dice {
     {
         this.random = new Random();
     }
-    public int roll()
+    public char chooseDice()
     {
         int randomValue = random.nextInt(100) + 1;
 
-        if (randomValue <= 20) return 1;
-        else if (randomValue <= 40) return 2;
-        else if (randomValue <= 60) return 3;
-        else return 4;
+        if (randomValue <= 25) return 'A';
+        else if (randomValue <= 50) return 'B';
+        else if (randomValue <= 75) return 'C';
+        else return 'D';
+    }
+
+    public int roll()
+    {
+        if (chooseDice() == 'A')
+        {
+            int randomValue = random.nextInt(100) + 1;
+            if (randomValue <= 20) return 1;
+            else if (randomValue <= 36) return 2;
+            else if (randomValue <= 52) return 3;
+            else if (randomValue <= 68) return 4;
+            else if (randomValue <= 84) return 5;
+            else return 6;
+        }
+        else if (chooseDice() == 'B')
+        {
+            int randomValue = random.nextInt(100) + 1;
+            if (randomValue <= 16) return 1;
+            else if (randomValue <= 36) return 2;
+            else if (randomValue <= 52) return 3;
+            else if (randomValue <= 68) return 4;
+            else if (randomValue <= 84) return 5;
+            else return 6;
+        }
+        else if (chooseDice() == 'C')
+        {
+            int randomValue = random.nextInt(100) + 1;
+            if (randomValue <= 16) return 1;
+            else if (randomValue <= 32) return 2;
+            else if (randomValue <= 52) return 3;
+            else if (randomValue <= 68) return 4;
+            else if (randomValue <= 84) return 5;
+            else return 6;
+        }
+        else
+        {
+            int randomValue = random.nextInt(100) + 1;
+            if (randomValue <= 16) return 1;
+            else if (randomValue <= 32) return 2;
+            else if (randomValue <= 48) return 3;
+            else if (randomValue <= 68) return 4;
+            else if (randomValue <= 84) return 5;
+            else return 6;
+        }
     }
 }
